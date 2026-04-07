4cc65b654f3e0a37af000bdc9b16819b2f14c38f
Ksaisjs <razanfnugroho.s@gmail.com>
2026-04-07 18:23:09 +0800

    fix: memperbaiki link direct imgur untuk logo

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ export default function App() {
                 {/* Team Logo */}
                 <div className="w-20 h-20 flex-shrink-0">
                   <img 
-                    src="https://i.imgur.com/sSt3y33.png" 
+                    src="https://i.imgur.com/Wryhn1q.png" 
                     alt="Layerwise Team Logo"
                     referrerPolicy="no-referrer"
                     className="w-full h-full object-contain drop-shadow-lg"
